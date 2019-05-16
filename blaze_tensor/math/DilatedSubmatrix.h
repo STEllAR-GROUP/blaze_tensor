2826a0b52bea58c3edd608dfc0ae23b12c7ca58f
Bita Hasheminezhad <bita.hn90@gmail.com>
2019-05-16 12:47:21 -0500

    Targeting library command updated

--- blaze_tensor/math/DilatedSubmatrix.h
+++ blaze_tensor/math/DilatedSubmatrix.h
@@ -5,6 +5,7 @@
 //
 //  Copyright (C) 2012-2019 Klaus Iglberger - All Rights Reserved
 //  Copyright (C) 2018-2019 Hartmut Kaiser - All Rights Reserved
+//  Copyright (C) 2019 Bita Hasheminezhad - All Rights Reserved
 //
 //  This file is part of the Blaze library. You can redistribute it and/or modify it under
 //  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
